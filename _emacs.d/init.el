65ca75e7b17c2c4e4b6524f968e6c15af52f482f
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2016-10-25 15:45:40 +0200

    enable magit

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -305,14 +305,14 @@
   ;:config
   ;(setq-default highlight-symbol-idle-delay 1.5))
 
-;(use-package magit
-  ;:ensure t
-  ;:defer t
-  ;:config
-  ;(setq magit-branch-arguments nil)
-  ;(setq magit-push-always-verify nil)
-  ;(setq magit-last-seen-setup-instructions "1.4.0")
-  ;(magit-define-popup-switch 'magit-log-popup ?f "first parent" "--first-parent"))
+(use-package magit
+  :ensure t
+  :defer t
+  :config
+  (setq magit-branch-arguments nil)
+  (setq magit-push-always-verify nil)
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  (magit-define-popup-switch 'magit-log-popup ?f "first parent" "--first-parent"))
 
 ;(use-package mmm-mode
   ;:ensure t

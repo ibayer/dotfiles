1be5538bcd73c405800506d0cfc32d8b1540e4a1
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2016-12-28 21:12:08 +0100

    add outline cycle to latex mode

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -200,6 +200,36 @@
   (define-key helm-map (kbd "S-SPC") 'helm-toggle-visible-mark)
   (define-key helm-find-files-map (kbd "C-k") 'helm-find-files-up-one-level))
 
+(use-package tex
+  :defer t
+  :ensure auctex
+  :config
+  (setq TeX-auto-save t)
+
+  (add-hook 'LaTeX-mode-hook 'outline-minor-mode)
+
+  ;; extra outline headers 
+  (setq TeX-outline-extra
+	'(("%chapter" 1)
+  ("%section" 2)
+  ("%subsection" 3)
+  ("%subsubsection" 4)
+  ("%paragraph" 5)))
+
+  ;; add font locking to the headers
+  (font-lock-add-keywords 'latex-mode
+  '(("^%\\(chapter\\|\\(sub\\|subsub\\)?section\\|paragraph\\)" 0 'font-lock-keyword-face t)
+  ("^%chapter{\\(.*\\)}"       1 'font-latex-sectioning-1-face t)
+  ("^%section{\\(.*\\)}"       1 'font-latex-sectioning-2-face t)
+  ("^%subsection{\\(.*\\)}"    1 'font-latex-sectioning-3-face t)
+  ("^%subsubsection{\\(.*\\)}" 1 'font-latex-sectioning-4-face t)
+  ("^%paragraph{\\(.*\\)}"     1 'font-latex-sectioning-5-face t)))
+
+  (eval-after-load 'outline
+  '(progn
+     (require 'outline-magic)
+     (define-key outline-minor-mode-map (kbd "<tab>") 'outline-cycle))))
+
 (use-package helm-bibtex
   :ensure t
   :config
@@ -352,12 +382,6 @@
   (setq magit-push-always-verify nil)
   (setq magit-last-seen-setup-instructions "1.4.0")
   (magit-define-popup-switch 'magit-log-popup ?f "first parent" "--first-parent"))
-
-(use-package tex
-  :defer t
-  :ensure auctex
-  :config
-  (setq TeX-auto-save t))
 
 ;(use-package mmm-mode
   ;:ensure t
